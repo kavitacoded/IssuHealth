739c32f6264d18b65bc2ba3e14336652d0bcbdc0
kavitacoded <kavi20900@gmail.com>
2021-09-14 15:19:45 +0530

    first commit

---
+++ src/main/java/hp/care/app/IssuHealthApplication.java
@@ -0,0 +1,13 @@
+package hp.care.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuHealthApplication.class, args);
+	}
+
+}
