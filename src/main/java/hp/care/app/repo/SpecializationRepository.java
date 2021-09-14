bbb754b845f131fb1bb69248cf059a001a60cf03
kavitacoded <kavi20900@gmail.com>
2021-09-14 15:46:45 +0530

    repo update

--- src/main/java/hp/care/app/repo/SpecializationRepository.java
+++ src/main/java/hp/care/app/repo/SpecializationRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import hp.care.app.entity.Specialization;
 
+
 public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
 
 
