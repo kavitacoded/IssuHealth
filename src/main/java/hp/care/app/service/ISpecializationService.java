f3b02532a9859557df779581ed16e0fa4281ab77
kavitacoded <kavi20900@gmail.com>
2021-09-14 17:07:29 +0530

    sericeimpl

---
+++ src/main/java/hp/care/app/service/ISpecializationService.java
@@ -0,0 +1,17 @@
+package hp.care.app.service;
+
+import java.util.List;
+
+import hp.care.app.entity.Specialization;
+
+public interface  ISpecializationService {
+
+	
+	public Long saveSpecialization(Specialization spec);
+	public List<Specialization> getAllSpecializations();
+	public void RemoveSpecialization(Long id);
+	public Specialization getOneSpecialization(Long id);
+	public void updateSpecialization(Specialization spec);
+	
+	
+}
--- src/test/java/hp/care/app/SpecializationRepositoryTest.java
+++ src/test/java/hp/care/app/SpecializationRepositoryTest.java
@@ -1,9 +1,16 @@
 package hp.care.app;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,11 +22,13 @@ import hp.care.app.repo.SpecializationRepository;
 @DataJpaTest(showSql=true)
 @AutoConfigureTestDatabase(replace=Replace.NONE)
 @Rollback(false)
+@TestMethodOrder(OrderAnnotation.class)
 public class SpecializationRepositoryTest {
 
 	private SpecializationRepository repo;
 	
 	//1. Test save operation
+	@Disabled
 	@Test
 	@Order(1)
 	public void testSpecCreate() {
@@ -30,7 +39,14 @@ public class SpecializationRepositoryTest {
 	}
 	
 	
-	
-	
 	//2. test display all operation
+	@Test
+	
+	@Order(2)
+	public void testSpeccFetchAll() {
+		List<Specialization>list=repo.findAll();
+		assertNotNull(list);
+		assertThat(list.size()<=0);
+		fail("no data exist in database");
+	}
 }
